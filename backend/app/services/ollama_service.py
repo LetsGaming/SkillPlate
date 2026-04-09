a5bf0744bccac864e18fa46b1127ab912031ffd9
sicraftertv <wehofskys@gmail.com>
2026-04-09 11:07:55 +0200

    Update `generate_course_content` to use `gemma4:e2b` model; update Dockerfile accordingly

--- backend/app/services/ollama_service.py
+++ backend/app/services/ollama_service.py
@@ -4,7 +4,7 @@ from ollama import ChatResponse
 client = ollama.Client(host="http://ollama:11434")
 
 
-def generate_course_content(theme: str, difficulty: str, duration: str, online: str, model: str = "gemma3:270m"):
+def generate_course_content(theme: str, difficulty: str, duration: str, online: str, model: str = "gemma4:e2b"):
     prompt = f"Erstelle einen Kurs mit Rezepten zu diesem Thema {theme} und dieser schwierigkeit {difficulty} und dieser länge {duration}"
     response: ChatResponse = client.chat(model=model, messages=[
         {
